e9f52fc1144430f76a0d994bf342b234fdbd7b05
darrylAlexander <mr.darryl.alexander@gmail.com>
2018-11-13 11:39:20 +0000

    Add: basic translation demo using i18next
    This is a basic demo with a sentence that is translated using i18next
    components. It also changes the language based on the button clicked.
    
    The languages are not currently abstracted out to files.
    
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/pages/TranslationDemo/TranslationDemo.tsx
    	new file:   src/utils/i18n.ts

--- src/pages/TranslationDemo/TranslationDemo.tsx
+++ src/pages/TranslationDemo/TranslationDemo.tsx
@@ -5,6 +5,10 @@ This is a simple placeholder page for use while testing translation components
 import * as React from 'react'
 import Button from '@material-ui/core/Button'
 
+import i18n from '../../utils/i18n';
+import { t } from 'i18next';
+import { withNamespaces } from 'react-i18next';
+
 // define the component properties with typing information for fields
 // properties are things that will have been passed down from parent component
 interface IProps {
@@ -25,10 +29,12 @@ export class TranslationDemoPage extends React.Component<IProps, IState> {
   }
   // functions required by the component can go here. Ideally complex business logic should be seperated out
   public changeLanguage = (lang: string) => {
-    this.setState({ language: lang })
+    this.setState({ language: lang });
+    i18n.changeLanguage(lang);
   }
   public reset = () => {
-    this.setState({ language: 'en' })
+    this.setState({ language: 'en' });
+    i18n.changeLanguage('en');
   }
   // here is where the main render method goes
   public render() {
@@ -39,8 +45,9 @@ export class TranslationDemoPage extends React.Component<IProps, IState> {
         {/* click functions can pass variables or just trigger */}
         <Button onClick={() => this.changeLanguage('en')}>English</Button>
         <Button onClick={() => this.changeLanguage('fr')}>French</Button>
-        <Button onClick={() => this.changeLanguage('sp')}>Spanish</Button>
+        <Button onClick={() => this.changeLanguage('es')}>Spanish</Button>
         <Button onClick={this.reset}>Reset</Button>
+        <h1>{t('Welcome to React')}</h1>
       </div>
     )
   }
---
+++ src/utils/i18n.ts
@@ -0,0 +1,37 @@
+import i18n from "i18next";
+import { reactI18nextModule } from "react-i18next";
+
+// the translations
+// (tip move them in a JSON file and import them)
+const resources = {
+  en: {
+    translation: {
+      "Welcome to React": "Welcome to React and react-i18next"
+    }
+  },
+  fr: {
+    translation: {
+      "Welcome to React": "Bienvenue a la React et react-i18next"
+    }
+  },
+  es: {
+    translation: {
+      "Welcome to React": "Bienvenido a React y react-i18next"
+    }
+  }
+};
+
+i18n
+  .use(reactI18nextModule) // passes i18n down to react-i18next
+  .init({
+    resources,
+    lng: "en",
+
+    keySeparator: false, // we do not use keys in form messages.welcome
+
+    interpolation: {
+      escapeValue: false // react already safes from xss
+    }
+  });
+
+export default i18n;
